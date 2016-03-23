360fe8e6c471f42def148335f1f06ade52c68eed
chanhou <chanhou@users.noreply.github.com>
2016-03-23 16:22:05 +0800

    Update app.js

--- app.js
+++ app.js
@@ -83,8 +83,8 @@ var app = express();
 // var user = mongoose.model('user'); // TODO [DB] : Get Vote model
 
 // all environments
-// app.set('port', process.env.PORT || 3000);
-app.set('port', process.env.VCAP_APP_PORT || 8000);
+app.set('port', process.env.PORT || 8000);
+// app.set('port', process.env.VCAP_APP_PORT || 8000);
 // app.set('port', process.env.PORT || 3000);
 //var app = express();
 //app.listen(process.env.VCAP_APP_PORT || 3000);
